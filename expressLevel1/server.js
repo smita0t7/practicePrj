78fcc7bf70ded3bd89b0038f85f01d797bd41fe3
SmitaSriyaSarangi <sdvmcsc@gmail.com>
2024-11-10 15:17:20 +0530

    ExpressAPI

--- expressLevel1/server.js
+++ expressLevel1/server.js
@@ -23,4 +23,3 @@ app.get('api', function(req,res){
 
 app.listen(PORT , ()=> console.log (`server is running on ${PORT}`))
 
-// this is a practice project to help me keep in track of the flow controll of this.
